8aea7d08d3ed591f7d0f18e14f9491b6bbe238e6
Dmitriy <dimamoon0@gmail.com>
2022-11-17 18:22:40 +1000

    new function added

--- SCOOPyGUI.py
+++ SCOOPyGUI.py
@@ -1,12 +1,12 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+import socket
+import threading
+import speech_recognition
 
 IsFirstTimeOpened = True
 USERNAME = ''
-
-
-def receiveMessage(message):
-    print(message)
+UDP_MAX_SIZE = 65535
 
 
 class Ui_Dialog(object):
@@ -47,35 +47,57 @@ class Ui_Dialog(object):
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(983, 634)
+        MainWindow.resize(990, 657)
+        MainWindow.setMaximumSize(QtCore.QSize(990, 657))
+        MainWindow.setStyleSheet("background-color: rgb(231, 255, 232);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.listWidget = QtWidgets.QListWidget(self.centralwidget)
-        self.listWidget.setGeometry(QtCore.QRect(50, 70, 881, 431))
+        self.listWidget.setGeometry(QtCore.QRect(0, 70, 991, 501))
+        self.listWidget.setMinimumSize(QtCore.QSize(991, 501))
+        self.listWidget.setStyleSheet("background-color: rgb(231, 255, 232);\n"
+                                      "font: 12pt \"MS Sans Serif\";")
         self.listWidget.setObjectName("listWidget")
-        self.titleLabel = QtWidgets.QLabel(self.centralwidget)
-        self.titleLabel.setGeometry(QtCore.QRect(720, 0, 261, 41))
-        self.titleLabel.setStyleSheet("font: 22pt \"Comic Sans MS\";")
-        self.titleLabel.setObjectName("titleLabel")
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(50, 510, 881, 31))
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(0, 580, 1001, 41))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.messageLine = QtWidgets.QTextEdit(self.horizontalLayoutWidget)
-        self.messageLine.setObjectName("messageLine")
-        self.horizontalLayout.addWidget(self.messageLine)
+        self.lineEdit = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
+        self.lineEdit.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout.addWidget(self.lineEdit)
+        self.toolButton = QtWidgets.QToolButton(self.horizontalLayoutWidget)
+        self.toolButton.setStyleSheet("background-color: rgb(252, 255, 39);")
+        self.toolButton.setObjectName("toolButton")
+        self.horizontalLayout.addWidget(self.toolButton)
         self.sendMessageButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.sendMessageButton.setStyleSheet("background-color: rgb(148, 255, 166);")
         self.sendMessageButton.setObjectName("sendMessageButton")
         self.horizontalLayout.addWidget(self.sendMessageButton)
-        self.userLabel = QtWidgets.QLabel(self.centralwidget)
-        self.userLabel.setGeometry(QtCore.QRect(10, 10, 661, 31))
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(0, -10, 991, 81))
+        self.frame.setStyleSheet("background-color: rgb(105, 255, 5);")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.titleLabel = QtWidgets.QLabel(self.frame)
+        self.titleLabel.setGeometry(QtCore.QRect(820, 20, 151, 41))
+        self.titleLabel.setStyleSheet("font: 22pt \"Comic Sans MS\";")
+        self.titleLabel.setObjectName("titleLabel")
+        self.userLabel = QtWidgets.QLabel(self.frame)
+        self.userLabel.setGeometry(QtCore.QRect(10, 30, 661, 31))
         self.userLabel.setStyleSheet("font: 18pt \"Comic Sans MS\";")
         self.userLabel.setObjectName("userLabel")
+        self.line = QtWidgets.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(0, 570, 991, 20))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 983, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 990, 26))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -85,12 +107,18 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        self.voice_to_text_button = QtWidgets.QPushButton('voice text', self)
+        self.voice_to_text_button.setGeometry(926, 620, 65, 20)
+        self.voice_to_text_button.setStyleSheet("background-color: green")
+        self.voice_to_text_button.clicked.connect(self.btn_clicked)
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "TEST GUI SCOOPy"))
-        self.titleLabel.setText(_translate("MainWindow", "SCOOPy BETA"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "SCOOPy"))
+        self.toolButton.setText(_translate("MainWindow", ":)"))
         self.sendMessageButton.setText(_translate("MainWindow", "SEND"))
-        self.userLabel.setText(_translate("MainWindow", ""))
+        self.titleLabel.setText(_translate("MainWindow", "SCOOPy"))
+        self.userLabel.setText(_translate("MainWindow", "USER"))
 
 
 class Dialog(QtWidgets.QDialog, Ui_Dialog):
@@ -122,6 +150,18 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.name = ''
         self.InitUI()
+        self.connect_to_server('localhost', 3001)
+        self.listen()
+        self.voice_recognizer = speech_recognition.Recognizer()
+
+    def connect_to_server(self, host: str, port: int) -> None:
+        self.host, self.port = host, port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.connect((self.host, self.port))
+        self.sock.send(f'__join{self.name}'.encode('utf-8'))
+
+    def listen(self):
+        threading.Thread(target=self.receiveMessages, daemon=True).start()
 
     def InitUI(self):
         global IsFirstTimeOpened
@@ -141,17 +181,43 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.name = USERNAME
 
     def sendMessages(self):
-        message = self.messageLine.toPlainText()
+        message = self.lineEdit.text()
         if message:
-            receiveMessage(message)
+            self.sock.send(f'{self.name}: {message}'.encode('utf-8'))
             self.listWidget.addItem('вы: ' + message)
-            self.messageLine.setText('')
+            self.lineEdit.setText('')
             return message
         else:
             return
 
-    def receiveMessages(self, *message):
-        self.listWidget.addItem(str(*message))
+    def receiveMessages(self):
+        while True:
+            msg = self.sock.recv(UDP_MAX_SIZE)
+            print(str(msg.decode('utf-8')))
+            self.listWidget.addItem(str(msg.decode('utf-8')))
+
+    def voice_to_text(self) -> str:
+        try:
+            with speech_recognition.Microphone(device_index=1) as source:
+                self.voice_recognizer.adjust_for_ambient_noise(source)
+                print('пытаюсь услышать')
+                audio = self.voice_recognizer.listen(source)
+            text = self.voice_recognizer.recognize_google(audio, language='ru-RU').lower()
+        except Exception:
+            return ''
+        return text
+
+    def change_voicebtn_color(self, color: str) -> None:
+        self.voice_to_text_button.setStyleSheet(f"background-color: {color}")
+
+    def voice_btn_pushed_thread(self):
+        self.change_voicebtn_color('red')
+        text = self.voice_to_text()
+        self.lineEdit.setText(text)
+        self.change_voicebtn_color('green')
+
+    def btn_clicked(self):
+        threading.Thread(target=self.voice_btn_pushed_thread).start()
 
 
 if __name__ == "__main__":

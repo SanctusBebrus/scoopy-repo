9de545421ffa103346dbf0e1446b1b1c5ff463d2
ezmoonlow <72476396+ezmoonlow@users.noreply.github.com>
2022-11-18 13:14:03 +1000

    Update Server.py

--- Server.py
+++ Server.py
@@ -4,7 +4,7 @@ UDP_MAX_SIZE = 65535
 members = []
 
 
-def listen(host: str = '127.0.0.1', port=3001):
+def listen(host: str = '127.0.0.1', port=55511):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     s.bind((host, port))
